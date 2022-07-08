ac28ee1b958e1d76c03b01417b8fb1213af8707d
TimurSafin27 <108293724+TimurSafin27@users.noreply.github.com>
2022-07-08 16:16:26 +0500

    Update Lesson3.java
    1

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -1,4 +1,60 @@
+class square
+{
+	double lenght;
+	
+	square (double l)
+	{
+	lenght = l;
+	}
+	
+	//Площадь
+	double area () 
+	{
+		return lenght*lenght;
+	}
+	
+	//Периметр
+	double Perimeter () 
+	{
+		return 4*lenght;
+	}
+	
+}
+
+class rectangle (double lenght, double width)
+{
+	//Площадь
+	double area () 
+	{
+		return lenght*width;
+	}
+	
+	//Периметр
+	double Perimeter () 
+	{
+		return 2*(lenght+width);
+	}
+}
+
+class hexagon (double lenght)
+{
+	//Площадь
+	double area () 
+	{
+		return 3*Math.sqrt(3)*lenght*lenght/2;
+	}
+	
+	//Периметр
+	double Perimeter () 
+	{
+		return 6*lenght;
+	}
+}
+
 public class Lesson3
 {
-
+    public static void main (String[] args)
+    {
+        //Потом
+    }
 }
